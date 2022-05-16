450746d17f927cb7c3011036dacab469915124a4
Francis Bolduc <f.bolduc@outlook.com>
2022-05-15 20:44:54 -0400

    rajout du try,catch

--- model/movies.js
+++ model/movies.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
-const MovieSherma = mongoose.Schema({}).add({
+const MovieSchema = mongoose.Schema({}).add({
     title: String,
     rated: String,
     user_id: String,
 })
 
-module.exports = mongoose.model("Movie", MovieSherma,);
+module.exports = mongoose.model("Movie", MovieSchema,);
--- moviesRoutes/homeControllers.js
+++ moviesRoutes/homeControllers.js
@@ -1,33 +1,40 @@
 const Movies = require("../model/movies");
 
-
-exports.movies = async (req, res)=>{
-    const title = req.query.title;
-    const rated = req.query.rated
-    const limite = parseInt(req.query.limite) || 20
-    const page = parseInt(req.query.page)
-
-    let results = await Movies.find({title: new RegExp(title, "i")})
-    .where("rated")
-    .equals(rated)
-    .limit(limite)
-    .skip(page)
-
-    res.json(results)
-}
-exports.rating = async (req, res)=>{
-
-    let ratings = await Movies.distinct("rated")
-
-    res.json(ratings)
+try{
+    exports.movies = async (req, res)=>{
+        const title = req.query.title;
+    
+        let results = await Movies.find({title: new RegExp(title, "i")})
+        .where("rated")
+        .equals(req.query.rated)
+        .limit(parseInt(req.query.limit) || 20)
+        .skip(parseInt(req.query.page))
+    
+        res.json(results)
+    }
+}catch(error){
+    console.log("Une erreur sais produite");
 }
 
-exports.searchById = async (req, res)=>{
-    const searchId = req.params.id
-    // const searchId = {_id : req.params.id}
+try {
+    exports.rating = async (req, res)=>{
 
-    // let oneMovie = await Movies.findById(searchId)
-    let oneMovie = await Movies.findById(searchId)
+        let ratings = await Movies.distinct("rated")
+    
+        res.json(ratings)
+    }
+} catch (error) {
+    console.log("Une erreur sais produite");
+}
 
-    res.json(oneMovie)
+try {
+    exports.searchById = async (req, res)=>{
+    
+        const searchId = req.params.id
+        let oneMovie = await Movies.findById(searchId)
+    
+        res.json(oneMovie)
+    }
+} catch (error) {
+    console.log("Une erreur sais produite");
 }
